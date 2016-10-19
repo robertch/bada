55b949e72715680288680ec1a852b79027ac0196
Robert Chojnacki <robchojnacki177@gmail.com>
2016-10-20 00:27:41 +0200

    usunięcie pliku konfiguracji

--- src/main/java/bada/httpclient/HTProxy.java
+++
@@ -1,8 +0,0 @@
-package bada.httpclient;
-
-public interface HTProxy {
-	public static String HOST = "poczta2.heuthesd";
-	public static int PORT = 8080;
-	public static String USER = "automat";
-	public static String PASSSWORD = "komitywa";
-}
